a7f7d4134b2502836563396e01d0516f9fbb152f
Kushagradalal1 <kushagradalal33@gmail.com>
2023-10-26 13:29:40 +0530

    Arrays Operations DSA

--- src/ArraysOpeartionsExample/ArraysExplanation.java
+++ src/ArraysOpeartionsExample/ArraysExplanation.java
@@ -1,2 +1,41 @@
-package ArraysOpeartionsExample;public class ArraysExplanation {
+package ArraysOpeartionsExample;
+/*
+Arrays Operations and Data Structures :
+Stores the multiple items of same type.
+int arr[]={10,20,30,40,50};
+In Array's we Stores the data into a contigous memory allocations
+x->address
+y->size
+
+x    x+y   x+2y   x+3y ....
+
+Advantages :
+
+1) Random Access is possible.-> we can access the element in the O(1).
+
+2) cache friendliness.
+
+
+Types of Arrays :
+1) Fixed Size Arrays.
+2) Dynamic Size Arrays.
+Java :
+
+*/
+public class ArraysExplanation {
+    public static void main(String[] args) {
+        //declaration of size nth is :
+        int n=100;
+        //fixed size arrays allocations:
+        int [] arr=new int[100];
+        //Arrays of size nth :
+        int [] arrOne=new int[n];
+        //normal declaration of arrays:
+        int arrTwo[]={10,20,30,40,50};
+
+        //dynamic size array in java :
+        //Example : ArrayList : resizing is possible in the dynamic size arrays:
+
+
+    }
 }
